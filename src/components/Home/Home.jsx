12c72928585281917b9b697d08d90db62f2fd2a7
MdNazmul12 <rifathosen7171@gmail.com>
2024-03-28 21:16:08 +0600

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,28 +5,67 @@ const Home = () => {
   return (
 
     <div>
-      <h2 className="text-3xl text-center mt-10 mb-10">Blood Bank Management System</h2>
-      <div className="grid grid-cols-4 gap-4 w-10/12 mx-auto">
-        <div className="bg-green-600 rounded-2xl">
-          <h4 className="text-3xl text-center text-white mt-6">Blood donors</h4>
-          <h2 className="text-6xl text-center text-white">400</h2>
+      <div className="">
+        <h2 className="text-3xl text-center mt-10 mb-10">Blood Bank Management System</h2>
+        <div className="grid md:grid-cols-4  sm:grid-cols-1 gap-4 w-10/12 mx-auto">
+          <div className="bg-sky-600 rounded-2xl">
+            <h4 className="text-3xl text-center text-white mt-6">Blood donors</h4>
+            <h2 className="text-6xl text-center text-white">400</h2>
+          </div>
+          <div className="bg-emerald-400 rounded-2xl">
+            <h4 className="text-3xl text-center  text-white mt-6">Reporting facilities</h4>
+            <h2 className="text-6xl text-center text-white">34</h2>
+          </div>
+          <div className="bg-green-600 rounded-2xl">
+            <h4 className="text-3xl text-center  text-white mt-6">Transfusion received</h4>
+            <h2 className="text-6xl text-center  text-white">2451</h2>
+          </div>
+          <div className="bg-rose-700 rounded-2xl">
+            <h4 className="text-3xl text-center  text-white mt-6">Reported transfusion </h4>
+            <h2 className="text-6xl text-center text-white">4</h2>
+          </div>
         </div>
-        <div className="bg-green-600 rounded-2xl">
-          <h4 className="text-3xl text-center  text-white mt-6">Reporting facilities</h4>
-          <h2 className="text-6xl text-center text-white">400</h2>
-        </div>
-        <div className="bg-green-600 rounded-2xl">
-          <h4 className="text-3xl text-center  text-white mt-6">Transfusion received</h4>
-          <h2 className="text-6xl text-center  text-white">400</h2>
-        </div>
-        <div className="bg-green-600 rounded-2xl">
-          <h4 className="text-3xl text-center  text-white mt-6">Reported transfusion </h4>
-          <h2 className="text-6xl text-center text-white">400</h2>
-        </div>
-        
-      
-        
 
+
+        <div>
+          <div>
+            <h2 className="text-3xl mt-10 text-center mb-10">Available blood around you</h2>
+          </div>
+          <div className=" text-center mb-10 grid grid-cols-2">
+           <h4 className="text-left ml-10">show</h4>
+           <h4 className="text-right text-2xl mr-6">Search:<input type="text"  className="input input-bordered w-full max-w-44" /></h4>
+          </div>
+          <table className="table-fixed grid w-989px">
+            <thead className="">
+              <tr className="font-bold text-2xl text-center grid grid-cols-4 gap-4">
+                <th className="">Facility Name</th>
+                <th>Blood Group</th>
+                <th>Blood Bag Number</th>
+                <th>Component</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="text-center mt-6 grid grid-cols-4 gap-4">
+                <td>Sir Salimullah Medical College Hospital</td>
+                <td>A+</td>
+                <td>1100</td>
+                <td>Whole Blood</td>
+              </tr>
+              <tr className="text-center mt-6 grid grid-cols-4 gap-4">
+                <td>Sir Salimullah Medical College Hospital</td>
+                <td>O+</td>
+                <td>1100</td>
+                <td>Whole Blood</td>
+              </tr>
+              <tr className="text-center mt-6 grid grid-cols-4 gap-4">
+                <td>Sir Salimullah Medical College Hospital</td>
+                <td>B+</td>
+                <td>3166</td>
+                <td>RCC</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
     //         <div className="carousel w-full">
